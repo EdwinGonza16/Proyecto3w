2c2db07e29b445f42a4a5c493fb06bc442f8b068
andres94-web <andresvallelap@gmail.com>
2024-05-14 12:20:08 -0500

    autentificacion agregada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Login } from "./components/login/Login";
+import { Logout } from "./components/logout/Logout";
+import { Profile } from "./components/profile/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isAuthenticated ? <Logout /> : <Login />}
+      <Profile />
     </div>
   );
 }
